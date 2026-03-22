7e9027b0a2fd7d190bb7c21894c27d7807c70d69
Todd Libby <toddlibby@protonmail.com>
2026-03-22 14:37:27 -0700

    fix: added public anon key

--- app/composables/useSupabase.ts
+++ app/composables/useSupabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   'https://gtctafjbikdrplrzzenq.supabase.co',
-  'sb_publishable_oO9FxHGHg_lpBUKK0zuKIA_bmcw8l5j'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0Y3RhZmpiaWtkcnBscnp6ZW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxOTA5MDAsImV4cCI6MjA4OTc2NjkwMH0.2AATvK4m4UrHZMj376oscYVMpjnPpXH5syq0IdSr15w'
 )
 
 export function useSupabase() {
